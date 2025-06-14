6d0470e0aa6d17c1226ca890d75e0d81da2dc3e3
Ben <69478311+mojo626@users.noreply.github.com>
2025-06-14 12:20:37 -0700

    config changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,75 @@
+'use client';
 import Image from "next/image";
 import styles from "./page.module.css";
+import TextField from '@mui/material/TextField';
+import Slider from "@mui/material/Slider";
+import React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CodeEditor from '@uiw/react-textarea-code-editor';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { useState } from "react";
 
 export default function Home() {
+  const [value, setValue] = React.useState<number>(30);
+
+  const handleChange = (event: Event, newValue: number) => {
+    setValue(newValue);
+  };
+
+
+  const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#000000',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+});
+
+  const [codeString, setCodeString] = useState("void int function");
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <ThemeProvider theme={theme}>
+          <TextField
+            multiline 
+            style={{backgroundColor: "gray", borderRadius: "5px", width: value*10}}
+            focused
+            color="primary"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setCodeString(event.target.value);
+            }}
+          />
+        </ThemeProvider>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+        
+        <SyntaxHighlighter language="cpp" style={docco}>
+      {codeString}
+    </SyntaxHighlighter>
+        
       </main>
+
+
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          style={{width: "1000px"}}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+          <Slider
+            value={value}
+            onChange={handleChange}
           />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples2
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        </div>
+        
       </footer>
     </div>
   );
